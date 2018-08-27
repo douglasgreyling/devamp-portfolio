b811d4e52aa0224d9363030e040ee76590c8d1c2
Douglas <greyling.douglas@gmail.com>
2018-08-27 14:15:01 +0200

    Built out initial form and partial call for comments along with controller actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_111857) do
+ActiveRecord::Schema.define(version: 2018_08_27_120308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2018_08_27_111857) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.text "Content"
+    t.text "content"
     t.bigint "user_id"
     t.bigint "blog_id"
     t.datetime "created_at", null: false
